fdcdd45bc1a7985fd89b83f4bfeb85eb653b56c8
Patrick Coffey <patrickcoffey505@gmail.com>
2020-12-31 16:37:06 -0600

    initial commit

---
+++ polls/views.py
@@ -0,0 +1,54 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+
+from .models import Question, Choice
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_q_list'
+
+    def get_queryset(self):
+        # return last five published questions
+        return Question.objects.filter(pub_date__lte = timezone.now()).order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        # excludes questions that haven't yet been published
+        return Question.objects.filter(pub_date__lte = timezone.now())
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+'''
+def index(request):
+    latest_q_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_q_list': latest_q_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request, 'polls/detail.html', {'question':question})
+'''
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # redisplay voting form
+        return render(request, 'polls/detail.html', {'question':question, 'error_message':"You didn't select a choice.",})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # return HttpResponseRedirect after success w Post data
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+'''
+def results(request, question_id):
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request, 'polls/results.html', {'question':question})
+'''
